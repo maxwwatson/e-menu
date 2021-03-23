01eeb7e45219906a87b4688a8f124d787314959e
Max Watson <max.w1@hotmail.com>
2021-03-22 22:54:16 -0500

    added modals

--- script.js
+++ script.js
@@ -19,3 +19,44 @@ function openCity(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
   } 
+
+// Code for Modals
+// Get the modal
+var modal = document.getElementsByClassName("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close");
+
+// When the user clicks on the button, open the modal
+btn[0].onclick = function() {
+  modal[0].style.display = "block";
+}
+
+btn[1].onclick = function() {
+  modal[1].style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span[0].onclick = function() {
+  modal[0].style.display = "none";
+}
+
+span[1].onclick = function() {
+  modal[1].style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  for(let i = 0; i < modal.length; i++) {
+    if(event.target == modal[i]) {
+      modal[i].style.display = "none";
+      break;
+    }
+  }
+  // if (event.target == modal[0]) {
+  //   modal[0].style.display = "none";
+  // }
+} 
